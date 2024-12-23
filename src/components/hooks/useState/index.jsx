4e3d45e98825410ba7ba5ede74777a9d9aab4812
Hassan-shah9 <hassan.thepearlsystem@gmail.com>
2024-12-23 11:37:00 +0700

    work  on condition

--- src/components/hooks/useState/index.jsx
+++ src/components/hooks/useState/index.jsx
@@ -3,14 +3,24 @@ import '../../Hooks.css';
 
 export const Counter = () => {
     const [count, setCount] = useState(0);
+    const [inputvalue, setInputValue] = useState(0);
     function handleCount() {
-        setCount(count + 1);
+        setCount((prevCount) => {
+            const newCount = prevCount + inputvalue;
+            if (newCount >= 100) return prevCount;
+            return newCount;
+        });
+        // console.log('inner:', count);
     }
+    console.log('outer:', count);
+
     function handleCountReverse() {
-        if (count <= 0) return; // Do not decrease below 0
-        setCount(count - 1);
+        setCount(count - inputvalue);
     }
 
+    function handleCountReset() {
+        setCount(0);
+    }
     return (
         <div
             className="container state-container"
@@ -19,12 +29,31 @@ export const Counter = () => {
             }}>
             <h1>useState Hook!</h1>
             <br />
-            <p>Count: {count}</p>
-            <button className="p-2 " onClick={handleCount}>
-                +
+            <p className="inline-block	">Count: {count}</p>
+            <br />
+            <input
+                className="border-2 p-2 mr-4"
+                type="text"
+                placeholder="Value"
+                value={inputvalue}
+                onChange={(e) => setInputValue(Number(e.target.value))}
+            />
+            <br />
+            <button
+                className="p-2 mt-4 "
+                onClick={handleCount}
+                disabled={count >= 100}>
+                Increment
             </button>
-            <button className="p-2 ml-2" onClick={handleCountReverse}>
-                -
+
+            <button
+                className="p-2 ml-2"
+                onClick={handleCountReverse}
+                disabled={count <= 0}>
+                Decrement
+            </button>
+            <button className="p-2 ml-2" onClick={handleCountReset}>
+                Reset
             </button>
         </div>
     );
